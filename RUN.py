a29d6113f97a1106df154ffed737f33e9b1a3580
viniciussgonzales <48864711+viniciussgonzales@users.noreply.github.com>
2019-03-23 19:16:13 -0300

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -11,8 +11,6 @@ class CustDrop(DropDown):
 
 kv_str = Builder.load_string('''
 #:import os os
-
-
 BoxLayout:
     orientation: 'vertical'
     BoxLayout:
@@ -23,31 +21,12 @@ BoxLayout:
             Color:
                 rgb: (1,1,1)
         size_hint_y:1
-
         Button:
             id: btn
-            text: 'test'
-            on_release: dropdown.open(self)
-            #size_hint_y: None
-            #height: '48dp'  
+            text: ''
+            on_release: os.system("python main_test.py")
 
-
-        CustDrop:
-
-            id: dropdown
-
-            Button:
-                text: 'Run another script'
-                size_hint_y: None
-                height: '48dp'
-                on_release: os.system("python main_test.py")
-
-        Label:
-            size_hint_x: 4
-
-    Label:
-        size_hint_y: 9
-
+                
 ''')
 
 
